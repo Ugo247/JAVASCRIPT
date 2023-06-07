d08f0a4873dd1448aec545b6dbb944fc2d1b35f0
Ugo247 <nelsonpotential81@gmail.com>
2023-06-07 04:20:07 -0700

    studied-function

--- script3.js
+++ script3.js
@@ -27,7 +27,46 @@ array = ["a", "b", "c", "d", "e", "f",];
 array.unshift("g", "1", "z");
 console.log(array);
 // Spread Operator
+array = ["a", "b", "c", "d", "e", "f",];
+let newArray = [...array, "g", "h", "i",];
+console.log(newArray);
 // reverse
+console.log(array.reserve());
 // find
+let students = [
+    { id: 12, name: "jane", sex: "f" },
+    { id: 13, name: "Rabo", sex: "m" },
+    { id: 34, name: "Gertrude", sex: "f" },
+    { id: 38, name: "Nonye", sex: "m" }
+];
+
+let student = studentsArray.find(x => x.id === 34);
+console.log(student);
 // filter
-// map
+let femaleStudents = studentArray.filter(x => x.gender === "f")
+console.log(femaleStudents);
+// map function => assignment
+
+// FUNCTIONS
+function doSomething(parameter) {
+    // write the code you want the function to run
+};
+
+const doSomething = (parameter1, parameter2) => {
+    // write the code you want the function to run
+};
+
+function addNumbers(x, y, z) {
+    console.log(x * y * z);
+}
+
+addNumbers(3, 5, 8);
+addNumbers(2, 15, 10);
+
+function pythagoras(opp, adj) {
+    // 1st Meth]od
+    console.log( opp**2 + adj**2 **0.5)
+    // 2nd Method
+    // console.log( Math.sqrt(opp**2 + adj**2) );
+};
+
